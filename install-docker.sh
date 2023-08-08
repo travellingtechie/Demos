615b7355b4f60f17d3fdad1320253817672af0c7
Brandon N <49294015+travellingtechie@users.noreply.github.com>
2023-08-08 17:52:56 -0600

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -4,3 +4,4 @@ sh get-docker.sh
 
 #install docker compose
 curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
