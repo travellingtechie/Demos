58d586500ef2d88cc66a94f0f415619d89dc5e27
Brandon N <49294015+travellingtechie@users.noreply.github.com>
2023-08-07 20:29:21 -0600

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+#install docker compose
+curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
